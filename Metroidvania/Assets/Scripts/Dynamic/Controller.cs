a8b908f8fbb24a1b7ac2bbf6c7dc6a3c589c1dab
Tobias Wolf <tobias.wolf-suessen@web.de>
2019-04-30 17:16:22 +0200

    Adding Controller and ControlelrState (for a movement state machine)

---
+++ Metroidvania/Assets/Scripts/Dynamic/Controller.cs
@@ -0,0 +1,85 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// This class controlls a "state machine" of a character
+/// </summary>
+[RequireComponent(typeof(Animator))]
+public class Controller : MonoBehaviour {
+
+    #region [MemberFields]
+
+    [SerializeField, Autohook]
+    private Animator f_animator;
+
+    [SerializeField]
+    private float f_height;
+
+    [SerializeField]
+    private float f_halfWidth;
+
+    #endregion
+
+    #region [PrivateVariables]
+
+    private ControllerState m_activeState;
+
+    #endregion
+
+    #region [Properties]
+
+    // collision results
+
+    public Animator Animator { get { return f_animator; } }
+    public float Height { get { return f_height; } }
+    public float HalfWidth { get { return f_halfWidth; } }
+
+    public Vector2 Velocity { get; set; }
+
+    #endregion
+
+    #region [Updates]
+
+    private void FixedUpdate() {
+
+        // check collisions
+        // check the active collisions
+        // ?
+
+        foreach (ControllerState state in m_activeState.FutureStates) {
+            bool entered = state.EnterOnCondition(m_activeState);
+            if (entered) {
+                Debug.Log("New active: " + state);
+                m_activeState = state;
+                return;
+            }
+        }
+
+        m_activeState.HandleFixedUpdate();
+
+        // Update position
+        // TODO; check for collisions
+
+        transform.position = transform.position + (Vector3)Velocity / 60f;
+
+    }
+
+    #endregion
+
+    #region [PublicMethods]
+
+    public void SetStartState(ControllerState state) {
+        m_activeState = state;
+    }
+
+    #endregion
+
+#if UNITY_EDITOR
+
+    private void OnDrawGizmos() {
+        m_activeState.OnDrawGizmos();
+    }
+
+#endif
+
+}
