7f5ade8954d59106c2ccb61bd372ebcd51d0fbe0
Tobias Wolf <tobias.wolf-suessen@web.de>
2019-05-17 12:29:15 +0200

    GenericEnemy.cs
    Holds Health and a Controller and will later also be given a behaviour

---
+++ Metroidvania/Assets/Scripts/Dynamic/Combat/GenericEnemy.cs
@@ -0,0 +1,55 @@
+using UnityEngine;
+
+public class GenericEnemy : MonoBehaviour, IDamagable {
+
+    #region [MemberFields]
+
+    //TODO all that stuff in SO
+
+    [SerializeField]
+    private int f_maxHealth;
+
+    [SerializeField]
+    private float f_weight;
+
+    [SerializeField]
+    private LootDrop f_loot;
+
+    #endregion
+
+    #region [FinalVariables]
+
+    [SerializeField, Autohook]
+    private Health f_health;
+
+    [SerializeField, Autohook]
+    private Controller f_controller;
+
+    #endregion
+
+    #region [PrivateVariables]
+
+    private float m_health;
+
+    #endregion
+
+    #region [Init]
+
+    private void Awake() {
+        m_health = f_maxHealth;
+
+        f_health.Init(f_maxHealth, f_weight);
+    }
+
+    #endregion
+
+    #region [Override]
+
+    public void TakeDamage(int amount, int healthAfter, int maxHealth, Vector2 hitNormal) {
+        //TODO
+        //f_controller.AbortSpecial / play Hit state
+    }
+
+    #endregion
+
+}
