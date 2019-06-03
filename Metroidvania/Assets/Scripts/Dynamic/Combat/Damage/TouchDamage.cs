8d95f8f6c762407bb234aca09862a5c314976620
Tobias Wolf <tobias.wolf-suessen@web.de>
2019-06-03 12:19:17 +0200

    Allowing Damage to hit nested colliders Allowing nested TouchDamage colliders

--- Metroidvania/Assets/Scripts/Dynamic/Combat/Damage/TouchDamage.cs
+++ Metroidvania/Assets/Scripts/Dynamic/Combat/Damage/TouchDamage.cs
@@ -11,6 +11,11 @@ public class TouchDamage : MonoBehaviour {
     [SerializeField]
     private Collider2D f_collider2;
 
+
+    //TODO; only use that (but that would break links)
+    [SerializeField]
+    private Collider2D[] f_colliders;
+
     [SerializeField]
     private EDamageReceiver f_eDamageReceiver;
 
@@ -22,24 +27,49 @@ public class TouchDamage : MonoBehaviour {
     #region [Updates]
 
     private void FixedUpdate() {
-        List<Collider2D> colliderList = DamageHelper.ContactList;
-        ContactFilter2D cf = new ContactFilter2D();
-        cf.SetLayerMask((int)f_eDamageReceiver);
-        cf.useLayerMask = true;
+        if (f_colliders != null && f_colliders.Length != 0) {
+            List<Collider2D> colliderList = DamageHelper.ContactList;
+            ContactFilter2D cf = new ContactFilter2D();
+            cf.SetLayerMask((int)f_eDamageReceiver);
+            cf.useLayerMask = true;
 
-        f_collider.OverlapCollider(cf, colliderList);
+            HashSet<Collider2D> colliderSet = new HashSet<Collider2D>();
 
-        //TODO; this is ugly and unnecessary if the other one is enabled
-        if (colliderList.Count == 0) {
-            f_collider2.OverlapCollider(cf, colliderList);
-        }
+            for (int i = 0; i < f_colliders.Length; ++i) {
+                f_colliders[i].OverlapCollider(cf, colliderList);
+                colliderSet.UnionWith(colliderList);
+            }
 
-        for (int i = 0; i < colliderList.Count; ++i) {
-            Health health = colliderList[i].GetComponent<Health>();
-            if (health) {
-                //TODO; this should still bump the toucher backwards a bit
-                //TODO; Also, I would still actually want the damaged HashSet since otherwise I kill other creatures very fast
-                health.TakeDamage(f_damage, Vector2.zero);
+            foreach (Collider2D collider in colliderSet) {
+                Health health = collider.GetComponent<Health>();
+                if (health) {
+                    //TODO; this should still bump the toucher backwards a bit
+                    //TODO; Also, I would still actually want the damaged HashSet since otherwise I kill other creatures very fast
+                    health.TakeDamage(f_damage, Vector2.zero);
+                }
+            }
+
+
+        } else {
+            List<Collider2D> colliderList = DamageHelper.ContactList;
+            ContactFilter2D cf = new ContactFilter2D();
+            cf.SetLayerMask((int)f_eDamageReceiver);
+            cf.useLayerMask = true;
+
+            f_collider.OverlapCollider(cf, colliderList);
+
+            //TODO; this is ugly and unnecessary if the other one is enabled
+            if (colliderList.Count == 0) {
+                f_collider2.OverlapCollider(cf, colliderList);
+            }
+
+            for (int i = 0; i < colliderList.Count; ++i) {
+                Health health = colliderList[i].GetComponent<Health>();
+                if (health) {
+                    //TODO; this should still bump the toucher backwards a bit
+                    //TODO; Also, I would still actually want the damaged HashSet since otherwise I kill other creatures very fast
+                    health.TakeDamage(f_damage, Vector2.zero);
+                }
             }
         }
     }
