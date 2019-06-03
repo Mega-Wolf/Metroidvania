0e9943e9b642937b6be020e26d27705078b44b38
Tobias Wolf <tobias.wolf-suessen@web.de>
2019-06-04 01:25:26 +0200

    Implementing AirMovAirMovement now uses a SmoothDamp

--- Metroidvania/Assets/Scripts/General/AirMovement.cs
+++ Metroidvania/Assets/Scripts/General/AirMovement.cs
@@ -39,7 +39,12 @@ public class AirMovement : Movement {
     #region [PublicMethods]
 
     public void Move(float speed) {
-        f_controller.Velocity = (Goal - (Vector2)f_controller.transform.position).normalized * speed;
+        //f_controller.Velocity = (Goal - (Vector2)f_controller.transform.position).normalized * speed;
+
+        Vector2 vel = f_controller.Velocity;
+
+        Vector2 goal = Vector2.SmoothDamp(f_controller.transform.position, Goal, ref vel, 0.25f, speed);
+        f_controller.Velocity = (goal - (Vector2)f_controller.transform.position).normalized * speed;
     }
 
     #endregion
