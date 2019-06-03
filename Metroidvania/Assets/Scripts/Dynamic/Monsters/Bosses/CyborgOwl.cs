1b145b2d0949a697f9b01b27a939d69d7970d988
Tobias Wolf <tobias.wolf-suessen@web.de>
2019-06-04 01:26:32 +0200

    CyborgOwl prefab + scripts
    (+ now actually adding those promised sprites)

---
+++ Metroidvania/Assets/Scripts/Dynamic/Monsters/Bosses/CyborgOwl.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+public class CyborgOwl : GenericEnemy {
+
+    #region [FinalVariables]
+
+    [SerializeField, Autohook] private FeatherFight f_featherFight;
+
+    #endregion
+
+    #region [Override]
+
+    protected override void Start() {
+        base.Start();
+
+        SetStartState(f_featherFight );
+    }
+
+    #endregion
+
+}
