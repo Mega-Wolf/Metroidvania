fcef38bb3a91f4074cf7ac6b891df5c6cac93493
Tobias Wolf <tobias.wolf-suessen@web.de>
2019-06-17 22:47:23 +0200

    DestructibleWall can enabl Monobehaviour when destroyed (e.g. unblock monster)

--- Metroidvania/Assets/Scripts/Dynamic/Monsters/DestructibleWall.cs
+++ Metroidvania/Assets/Scripts/Dynamic/Monsters/DestructibleWall.cs
@@ -2,10 +2,31 @@ using UnityEngine;
 
 public class DestructibleWall : MonoBehaviour, IDamageTaker {
 
+    #region [MemberFields]
+
+    [SerializeField] private MonoBehaviour[] f_blocked;
+
+    #endregion
+
+    #region [Init]
+
+    private void Start() {
+        for (int i = 0; i < f_blocked.Length; ++i) {
+            f_blocked[i].enabled = false;
+        }
+    }
+
+    #endregion
+
     #region [Override]
+
+    public Controller Controller { get { return null; } }
 
     public void TakeDamage(int amount, Vector2 hitNormal) {
         Destroy(gameObject);
+        for (int i = 0; i < f_blocked.Length; ++i) {
+            f_blocked[i].enabled = true;
+        }
     }
 
     #endregion
