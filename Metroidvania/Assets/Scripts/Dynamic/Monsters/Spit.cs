bde53f155320da86ed746fa43184e83246f86fda
Tobias Wolf <tobias.wolf-suessen@web.de>
2019-06-17 22:46:48 +0200

    Added code to spit that it reacts on touching different stuff With VisualExplosion

--- Metroidvania/Assets/Scripts/Dynamic/Monsters/Spit.cs
+++ Metroidvania/Assets/Scripts/Dynamic/Monsters/Spit.cs
@@ -1,12 +1,23 @@
+using System.Collections.Generic;
 using UnityEngine;
 using WolfBT;
 
-public class Spit : MonoBehaviour {
+public class Spit : MonoBehaviour, IDamager {
 
     #region [MemberFields]
 
     [SerializeField] private float f_speed;
     [SerializeField] private float f_b;
+    [SerializeField] private int f_damageValue;
+    [SerializeField] private int f_cooldownExplosionFrames;
+    [SerializeField] private int f_explodeFrameCount;
+
+    #endregion
+
+    #region [FinalVariables]
+
+    [SerializeField, Autohook] private Damage f_damage;
+    [SerializeField, Autohook] private Animator f_aniamtor;
 
     #endregion
 
@@ -15,28 +26,23 @@ public class Spit : MonoBehaviour {
     private float m_a;
     private float m_b;
     private bool m_right;
+    private bool m_shallMove = true;
+    private int m_explodeCooldown;
+    private Vector3 m_originalScale;
 
     #endregion
 
     #region [Init]
 
+    private void OnValidate() {
+        if (f_damage) {
+            f_damage.Init(EDamageReceiver.Player | EDamageReceiver.Default, -1, this, false, true);
+        }
+    }
+
     private void Start() {
-        // f_behaviourTree = new BehaviourTree(
-        //     //Only collide with statics since normal damage will be covered by Damage
-        //     new Sequence(
-        //         new Collide(f_collider, contactFilter),
-        //         new ActionGroup(
-        //             () => {
-        //                 for (int i = 0; i < DamageHelper.ContactList.Count; ++i) {
-
-        //                 }
-        //             }
-        //         )
-        //     )
-
-        // );
-
-        //f_behaviourTree.Enter();
+        OnValidate();
+        f_damage.ExecuteHit(f_damageValue, Vector2.zero);
     }
 
     #endregion
@@ -44,8 +50,25 @@ public class Spit : MonoBehaviour {
     #region [Updates]
 
     private void FixedUpdate() {
-        float newX = transform.localPosition.x + f_speed / 50f * (m_right ? 1 : -1);
-        transform.localPosition = new Vector3(newX, newX * (m_a * newX + m_b) , transform.localPosition.z);
+        if (m_shallMove) {
+            float newX = transform.localPosition.x + f_speed / 50f * (m_right ? 1 : -1);
+            transform.localPosition = new Vector3(newX, newX * (m_a * newX + m_b), transform.localPosition.z);
+        } else {
+            --m_explodeCooldown;
+            if (m_explodeCooldown == 0) {
+                f_aniamtor.enabled = true;
+                f_aniamtor.Play("Explode");
+            }
+            if (m_explodeCooldown > 0) {
+                
+                Vector2 pseudoGoal = Vector3.one * (0.15f * Mathf.Sin(5 * 2 * Mathf.PI * (m_explodeCooldown / (float)f_cooldownExplosionFrames)) + 1);
+                Vector2 goal = pseudoGoal * m_originalScale;
+                transform.localScale = new Vector3(goal.x, goal.y, m_originalScale.z);
+            }
+            if (m_explodeCooldown == -f_explodeFrameCount * 5) {
+                Destroy(transform.gameObject);
+            }
+        }
     }
 
     #endregion
@@ -70,6 +93,40 @@ public class Spit : MonoBehaviour {
         }
 
         m_right = playerPos.x > 0;
+    }
+
+    #endregion
+
+    #region [Override]
+
+    public void Damaged(IDamageTaker damageTaker) {
+
+        //f_damage.Abort();
+        m_shallMove = false;
+
+        if (damageTaker == null) {
+            // since I don't have the contact point anymore; i just create it again
+            List<Collider2D> colliders = DamageHelper.ContactList;
+            // This is not generally correct
+
+            Transform oldParent = transform.parent;
+
+            transform.SetParent(colliders[0].transform, true);
+            transform.localRotation = Quaternion.identity;
+            transform.position = transform.position + Vector3.back * 3;
+
+            Destroy(oldParent.gameObject);
+
+            m_originalScale = transform.localScale;
+            m_explodeCooldown = f_cooldownExplosionFrames;
+        } else {
+            if (transform.parent.name.StartsWith("Spit")) {
+                Destroy(transform.parent.gameObject);
+            } else {
+                Destroy(transform.gameObject);
+            }
+            
+        }
     }
 
     #endregion
