7c4d185c1b7e97da03c7765785aa12059660e757
Tobias Wolf <tobias.wolf-suessen@web.de>
2019-06-18 16:02:38 +0200

    Adding VisualHit prefab, sprites etc.

---
+++ Metroidvania/Assets/Scripts/Dynamic/TimedDestroy.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TimedDestroy : MonoBehaviour {
+
+    #region [MemberFields]
+
+    [SerializeField] private int f_destructionTimer;
+
+    #endregion
+
+    #region [Updates]
+
+    private void FixedUpdate() {
+        --f_destructionTimer;
+        if (f_destructionTimer <= 0) {
+            Destroy(gameObject);
+        }
+    }
+
+    #endregion
+}
